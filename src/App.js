d77f60a2a309adf2470f1afc09fde36f106b75cd
raiab <abhishekr@dailyprophet.ai>
2020-10-10 08:49:25 +0530

    imported trimmed_keypoints and tried to use data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import trimmed_keypoints from "./trimmed_keypoints.json";
 import "./App.css";
 
 function App() {
+  // console.log("data=", trimmed_keypoints.info);
+  const [images] = useState(trimmed_keypoints.images);
+  // images.forEach((image) => {
+  //   console.log(image.file_name);
+  // });
   return (
     <div className="App">
-      <h1> LEDO </h1>
+      <h1>size of images array is:- {images.length}</h1>
+      <ul>
+        {images.map((image) => {
+          return <li>{image.file_name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
