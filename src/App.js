93a366773a0e0cfdd78c6f4c909526aa0b8d47af
raiab <abhishekr@dailyprophet.ai>
2020-10-11 12:32:28 +0530

    made a basic table

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
-import trimmed_keypoints from "./trimmed_keypoints.json";
+import React from "react";
 import "./App.css";
-
+import Table from "./components/Table";
 function App() {
-  // console.log("data=", trimmed_keypoints.info);
-  const [images] = useState(trimmed_keypoints.images);
-  // images.forEach((image) => {
-  //   console.log(image.file_name);
-  // });
   return (
     <div className="App">
-      <h1>size of images array is:- {images.length}</h1>
-      <ul>
-        {images.map((image) => {
-          return <li>{image.file_name}</li>;
-        })}
-      </ul>
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import trimmed_keypoints from "../data/trimmed_keypoints.json";
+function Table() {
+  const [images] = useState(trimmed_keypoints.images);
+  const [attributesName] = useState(Object.keys(images[0]));
+
+  console.log(attributesName);
+  return (
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            {attributesName.map((attribute) => {
+              return <th key={attribute}> {attribute} </th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {images.map((image) => {
+            return (
+              <tr key={image.id}>
+                {attributesName.map((attribute) => {
+                  return <td> {image[attribute]} </td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
