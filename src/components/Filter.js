ca9823cc5670a33db1e9d6ce04cabde8219ec204
raiab <abhishekr@dailyprophet.ai>
2020-10-11 14:48:08 +0530

    Added Filter options in Filter container

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,24 @@
-import React from "react";
-function filter() {
-  console.log("I Filter component");
-  return <h3> In Filter component</h3>;
+import React, { useState } from "react";
+function Filter() {
+  const [filter, setFilter] = useState("none");
+  function handleFilterChange(event) {
+    const [value] = event.target;
+    setFilter(value);
+    console.log("filter is=", filter);
+  }
+  console.log("In Filter component");
+  return (
+    <select value={filter} onChange={handleFilterChange}>
+      <option value="">{filter === "none" ? "Add Filter" : { filter }}</option>
+      <option value="license">License</option>
+      <option value="file_name">File_name</option>
+      <option value="coco_url">Coco_url</option>
+      <option value="height">Height</option>
+      <option value="width">Width </option>
+      <option value="date_captures">Date_captures </option>
+      <option value="flikr_url">Flikr_url </option>
+      <option value="id">Id </option>
+    </select>
+  );
 }
-export default filter;
+export default Filter;
