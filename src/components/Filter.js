5156d60f1d7156722aefdea6f117d7611cbec59a
raiab <abhishekr@dailyprophet.ai>
2020-10-11 14:27:39 +0530

    added buttons and Filter comment container

---
+++ src/components/Filter.js
@@ -0,0 +1,6 @@
+import React from "react";
+function filter() {
+  console.log("I Filter component");
+  return <h3> In Filter component</h3>;
+}
+export default filter;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,45 +1,58 @@
 import React, { useState } from "react";
 import trimmed_keypoints from "../data/trimmed_keypoints.json";
+import Filter from "./Filter";
 function Table() {
   const [images] = useState(trimmed_keypoints.images);
   const [attributesName] = useState(Object.keys(images[0]));
-
+  const [showFilter, setShowFilter] = useState(false);
   console.log(attributesName);
+  function handleFilterClick() {
+    setShowFilter(true);
+  }
   return (
-    <div className="table-container">
-      <table className="table">
-        <thead>
-          <tr>
-            {attributesName.map((attribute) => {
-              return <th key={attribute}> {attribute} </th>;
+    <>
+      <div className="main-buttons">
+        <button onClick={handleFilterClick}> Filter </button>
+
+        <button>Sorting</button>
+        <button>Display Setting </button>
+        {showFilter && <Filter />}
+      </div>
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              {attributesName.map((attribute) => {
+                return <th key={attribute}> {attribute} </th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {images.map((image) => {
+              return (
+                <tr key={image.id}>
+                  {attributesName.map((attribute) => {
+                    if (attribute === "coco_url" || attribute === "flickr_url")
+                      return (
+                        <td>
+                          {" "}
+                          <img
+                            src={image[attribute]}
+                            alt={attribute}
+                            height="200"
+                            width="200"
+                          />{" "}
+                        </td>
+                      );
+                    else return <td> {image[attribute]} </td>;
+                  })}
+                </tr>
+              );
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {images.map((image) => {
-            return (
-              <tr key={image.id}>
-                {attributesName.map((attribute) => {
-                  if (attribute === "coco_url" || attribute === "flickr_url")
-                    return (
-                      <td>
-                        {" "}
-                        <img
-                          src={image[attribute]}
-                          alt={attribute}
-                          height="200"
-                          width="200"
-                        />{" "}
-                      </td>
-                    );
-                  else return <td> {image[attribute]} </td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
